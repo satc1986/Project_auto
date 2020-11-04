c11d4b9ea1ccb72eeedb745d3c4cd4e7f380374a
Darshan <test@gmail.com>
2020-11-04 14:49:54 -0600

    lot's of bugs fixed

---
+++ config.py
@@ -0,0 +1,3 @@
+username = "satc1986"
+password = "Sndb130606@"
+path = "C:/Darshan/code/"
--- create.py
+++ create.py
@@ -1,19 +1,34 @@
 import sys
 import os
-from github import Github
+from selenium import webdriver
 from dotenv import load_dotenv
+import config
 
 load_dotenv()
 
-path = os.getenv("FILEPATH")
-username = "satc1986"
-password = "India123^"
+
+webdriver = webdriver.Chrome("chromedriver.exe")
 
 def create():
     folderName = str(sys.argv[1])
-    os.makedirs(path + str(folderName))
-    user = Github(username, password).get_user()
-    repo = user.create_repo(folderName)
+    os.makedirs(config.path + str(folderName))
+    webdriver.get("https://github.com/")
+    b1 = webdriver.find_element_by_xpath('/html/body/div[1]/header/div/div[2]/div[2]/a[1]')
+    b1.click()
+    t1 = webdriver.find_element_by_id("login_field")
+    t1.send_keys(config.username)
+    t2 = webdriver.find_element_by_id("password")
+    t2.send_keys(config.password)
+    b3 = webdriver.find_element_by_xpath('//*[@id="login"]/form/div[4]/input[12]')
+    b3.click()
+    button = webdriver.find_element_by_xpath('//*[@id="repos-container"]/h2/a')
+    button.click()
+    textbox = webdriver.find_element_by_xpath('//*[@id="new_repository"]/div[2]/auto-check/dl/dd')
+    textbox.send_keys(folderName)
+    button2 = webdriver.find_element_by_xpath('//*[@id="repository_auto_init"]')
+    button2.click()
+    finishedButton = webdriver.find_element_by_xpath('//*[@id="new_repository"]/div[4]/button')
+    finishedButton.click()
     print("Succesfully created repository {}".format(folderName))
 
 if __name__ == "__main__":
